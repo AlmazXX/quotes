1ecb88885479f19a2bfc2c71f6ef90f0dd04b12d
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-03 14:17:04 +0600

    Added layout to App Added Navbar, Routes, Route comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./containers/Home/Home";
+import Add from "./containers/Quotes/Add";
 
 function App() {
   return (
     <>
-      <header className=""></header>
-      <main></main>
+      <header>
+        <Navbar/>
+      </header>
+      <main className="container">
+        <Routes>
+          <Route path="/"element={<Home/>} />
+          <Route path="/quotes"element={<Home/>} />
+          <Route path="/quotes/add"element={<Add/>} />
+          <Route path="*"element={<div className="row mt-3"><h4>Not found!</h4></div>} />
+        </Routes>
+      </main>
     </>
   );
 }
