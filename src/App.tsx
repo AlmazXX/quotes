39fafe4bf69946a2fd5c948206f7bba646757be6
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-03 17:23:47 +0600

    Added paths to /quotes/:id, /quotes/:id/edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./containers/Home/Home";
 import Add from "./containers/Quotes/Add";
+import Edit from "./containers/Quotes/Edit";
+import Quotes from "./containers/Quotes/Quotes";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
       </header>
       <main className="container">
         <Routes>
-          <Route path="/"element={<Home/>} />
-          <Route path="/quotes"element={<Home/>} />
+          <Route path="/"element={<Quotes/>} />
+          <Route path="/quotes"element={<Quotes/>} />
+          <Route path="/quotes/:id"element={<Quotes/>} />
+          <Route path="/quotes/:id/edit"element={<Edit/>} />
           <Route path="/quotes/add"element={<Add/>} />
           <Route path="*"element={<div className="row mt-3"><h4>Not found!</h4></div>} />
         </Routes>
