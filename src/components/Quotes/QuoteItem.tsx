56a2d4d9477868132716998660df5a1a37e102d6
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-03 17:20:01 +0600

    Updated QuoteItem Added categories, onQuoteDelete props Added buttons for deleting and editing quotes

--- src/components/Quotes/QuoteItem.tsx
+++ src/components/Quotes/QuoteItem.tsx
@@ -1,28 +1,29 @@
-import { FC } from "react";
-import { Quote } from "../../types";
+import { FC, MouseEventHandler } from "react";
+import { Link } from "react-router-dom";
+import { IQuote } from "../../types";
 
 interface Props {
-  quote: Quote;
+  quote: IQuote;
+  categories: { [key: string]: string };
+  onQuoteDelete: MouseEventHandler;
 }
 
-const QuoteItem: FC<Props> = ({ quote }) => {
+const QuoteItem: FC<Props> = ({ quote, categories, onQuoteDelete }) => {
   return (
     <div className="col-12">
       <div className="card">
         <div className="card-body">
           <p className="small float-sm-end m-0 text-muted">
-            Category: <span>{quote.category}</span>
+            Category: <span>{categories[quote.category]}</span>
           </p>
           <h5 className="card-title">{quote.author}</h5>
           <p className="card-text">{quote.text}</p>
-          {/* <Link
-            to={
-              location.pathname === "/posts" ? `${post.id}` : `posts/${post.id}`
-            }
-            className="btn btn-primary"
-          >
-            Read more
-          </Link> */}
+          <div className="d-flex gap-3 px-0 mb-3">
+            <Link to={`/quotes/${quote.id}/edit`} className="btn btn-primary">Edit</Link>
+            <button className="btn btn-danger" onClick={onQuoteDelete}>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
