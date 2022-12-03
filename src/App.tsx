9627ca73a21552d6cca61c51d9bbfa73d2d24053
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-03 17:37:12 +0600

    Moved Add, Edit conts to their dirs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Add from "./containers/Quotes/Add";
-import Edit from "./containers/Quotes/Edit";
-import Quotes from "./containers/Quotes/Quotes";
+import Add from "./containers/Add/Add";
+import Edit from "./containers/Edit/Edit";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       </header>
       <main className="container">
         <Routes>
-          <Route path="/"element={<Quotes/>} />
-          <Route path="/quotes"element={<Quotes/>} />
-          <Route path="/quotes/:id"element={<Quotes/>} />
+          <Route path="/"element={<Home/>} />
+          <Route path="/quotes"element={<Home/>} />
+          <Route path="/quotes/:id"element={<Home/>} />
           <Route path="/quotes/:id/edit"element={<Edit/>} />
           <Route path="/quotes/add"element={<Add/>} />
           <Route path="*"element={<div className="row mt-3"><h4>Not found!</h4></div>} />
